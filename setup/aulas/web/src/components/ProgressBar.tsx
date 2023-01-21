96d655a1d08d4ce0dfd4018d0c19b11a703c02e3
YohanBZ <barbozayohan@gmail.com>
2023-01-21 12:07:53 -0300

    aula5-web

--- setup/aulas/web/src/components/ProgressBar.tsx
+++ setup/aulas/web/src/components/ProgressBar.tsx
@@ -3,17 +3,15 @@ interface ProgressBarProps {
 }
 
 export function ProgressBar(props: ProgressBarProps) {
-  const progressStyles = {
-    width: `${props.progress}%`
-  }
+ 
   return(
     <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
       <div 
       role="progressbar"
       aria-label="Progresso de hábitos completos nesse dia"
       aria-valuenow={props.progress}
-      className="h-3 rounded-xl bg-violet-600"
-      style={progressStyles}
+      className="h-3 rounded-xl bg-violet-600 transition-all"
+      style={{ width: `${props.progress}%`}}
       />
     </div>
   )
